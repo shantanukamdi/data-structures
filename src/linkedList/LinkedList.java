422f89b99ae4fff1a4afa4423a70ec808ef0476b
Shantanu Kamdi <kamdishantanu@gmail.com>
2018-09-24 07:43:54 +0530

    Insertion in linked list: Complete program

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -19,6 +19,29 @@ public class LinkedList {
 		}
 	}
 	
+	public void insertAtStart(int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		node.next = head;
+		head = node;
+	}
+	
+	public void insertAt(int data, int pos) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		
+		Node n = head;
+		int count = 0;
+		while(count < pos-1) {
+			n = n.next;
+			count++;
+		}
+		node.next = n.next;
+		n.next = node;
+	}
+	
 	public void printList() {
 		Node n = head;
 		while(n!=null) {
--- src/linkedList/Runner.java
+++ src/linkedList/Runner.java
@@ -5,12 +5,21 @@ public class Runner {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		
+		// Insert at end position
 		list.insert(12);
 		list.insert(15);
 		list.insert(22);
 		
 		list.printList();
-
+		System.out.println("----");
+		// Insert at start position
+		list.insertAtStart(1);
+		list.printList();
+		System.out.println("----");
+		// Insert at any position
+		list.insertAt(2, 3);
+		list.printList();
+		
 	}
 
 }
