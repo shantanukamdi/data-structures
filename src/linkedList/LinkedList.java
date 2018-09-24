ea9cf45c6d5a7cc57e1761e9fc3ac3d9cbb2d029
Shantanu Kamdi <kamdishantanu@gmail.com>
2018-09-24 08:00:18 +0530

    Insertion and Deletion in linked list: Complete program

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -2,23 +2,23 @@ package linkedList;
 
 public class LinkedList {
 	Node head;
-	
+
 	public void insert(int data) {
 		Node node = new Node();
 		node.data = data;
 		node.next = null;
-		
-		if(head == null) {
+
+		if (head == null) {
 			head = node;
 		} else {
 			Node n = head;
-			while(n.next!=null) {
+			while (n.next != null) {
 				n = n.next;
 			}
 			n.next = node;
 		}
 	}
-	
+
 	public void insertAtStart(int data) {
 		Node node = new Node();
 		node.data = data;
@@ -26,28 +26,52 @@ public class LinkedList {
 		node.next = head;
 		head = node;
 	}
-	
+
 	public void insertAt(int data, int pos) {
 		Node node = new Node();
 		node.data = data;
 		node.next = null;
-		
-		Node n = head;
-		int count = 0;
-		while(count < pos-1) {
-			n = n.next;
-			count++;
+
+		if (pos == 0) {
+			insertAtStart(data);
+		} else {
+			Node n = head;
+			int count = 0;
+			while (count < pos - 1) {
+				n = n.next;
+				count++;
+			}
+			node.next = n.next;
+			n.next = node;
 		}
-		node.next = n.next;
-		n.next = node;
+
 	}
-	
+
 	public void printList() {
 		Node n = head;
-		while(n!=null) {
+		while (n != null) {
 			System.out.println(n.data);
 			n = n.next;
 		}
-		
+
 	}
+
+	public void deleteAt(int pos) {
+		if (pos == 0) {
+			head = head.next;
+		} else {
+			Node n = head;
+			Node n1 = null;
+			int count = 0;
+			while (count < pos - 1) {
+				n = n.next;
+				count++;
+			}
+			n1 = n.next;
+			n.next = n1.next;
+			System.out.println("Deleting " + n1.data);
+			n1 = null;
+		}
+	}
+
 }
--- src/linkedList/Runner.java
+++ src/linkedList/Runner.java
@@ -4,22 +4,26 @@ public class Runner {
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-		
+		// List is: 1->12->15->22
 		// Insert at end position
 		list.insert(12);
 		list.insert(15);
 		list.insert(22);
-		
-		list.printList();
-		System.out.println("----");
-		// Insert at start position
+
 		list.insertAtStart(1);
-		list.printList();
-		System.out.println("----");
-		// Insert at any position
-		list.insertAt(2, 3);
-		list.printList();
 		
+		list.printList();
+		// System.out.println("----");
+		// // Insert at start position
+		// list.insertAtStart(1);
+		// list.printList();
+		// System.out.println("----");
+		// // Insert at any position
+		// list.insertAt(2, 3);
+		// list.printList();
+		System.out.println("---");
+		list.deleteAt(2);
+		list.printList();
 	}
 
 }
